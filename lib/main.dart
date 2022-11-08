13dba0037882551d52b17922de350a7862381d47
Exxsid <cortezleoanthony5@gmail.com>
2022-11-08 08:33:50 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {  
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(1),
+      child: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
